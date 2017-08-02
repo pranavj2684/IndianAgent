54e7df1c8652820d4c3eca17b06526bdbfc0608e
Wasim Sayyad <wasims831@gmail.com>
2017-08-02 21:03:15 +0530

    Get Aadhar card data

--- app/src/main/java/in/itechvalley/indianagent/QRCodeScanning.java
+++ app/src/main/java/in/itechvalley/indianagent/QRCodeScanning.java
@@ -27,12 +27,17 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import in.itechvalley.indianagent.Model.DataAttributes;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QRCodeScanning extends AppCompatActivity implements ZXingScannerView.ResultHandler
 {
 
     private ZXingScannerView mScannerView;
+
+
+    String uid,name,gender,yearOfBirth,careOf,villageTehsil,postOffice,district,state,postCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -60,46 +65,137 @@ public class QRCodeScanning extends AppCompatActivity implements ZXingScannerVie
         builder.setMessage(result.getText());
         AlertDialog dialog = builder.create();
         Log.d("TAG", "handleResult:Scan Result QR Code+++ " + result);
+        Log.d("TAG", "handleResult:Scan Result QR Code+++ " + result.getBarcodeFormat());
 
-        try
-        {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xpp = factory.newPullParser();
+        processScannedData(result.getText());
 
-            xpp.setInput(new StringReader(result.getText()));
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT)
-            {
-                if (eventType == XmlPullParser.START_DOCUMENT)
-                {
-                    System.out.println("Start document");
-                }
-                else if (eventType == XmlPullParser.START_TAG)
-                {
-                    System.out.println("Start tag " + xpp.getName());
-                }
-                else if (eventType == XmlPullParser.END_TAG)
-                {
-                    System.out.println("End tag " + xpp.getName());
-                }
-                else if (eventType == XmlPullParser.TEXT)
-                {
-                    System.out.println("Text " + xpp.getText());
-                    System.out.println("Text " + xpp.getNamespace());
 
-                }
-                eventType = xpp.next();
-            }
-            System.out.println("End document");
-        }
-        catch (XmlPullParserException | IOException e)
-        {
-            e.printStackTrace();
-        }
+
+
+
+
+//        try
+//        {
+//            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+//            factory.setNamespaceAware(true);
+//            XmlPullParser xpp = factory.newPullParser();
+//
+//            xpp.setInput(new StringReader(result.getText()));
+//            int eventType = xpp.getEventType();
+//
+//            while (eventType != XmlPullParser.END_DOCUMENT)
+//            {
+//                String Name=xpp.getName();
+//                if (eventType == XmlPullParser.START_DOCUMENT)
+//                {
+//                    System.out.println("Start document");
+//                }
+//                else if (eventType == XmlPullParser.START_TAG)
+//                {
+//                    System.out.println("Start tag " + xpp.getName());
+//                }
+//                else if (eventType == XmlPullParser.END_TAG)
+//                {
+//                    System.out.println("End tag " + xpp.getName());
+//
+//                    if (Name.equals("name"))
+//                    {
+//
+//                    }
+//                }
+//                else if (eventType == XmlPullParser.TEXT)
+//                {
+//                    System.out.println("Text " + xpp.getText());
+//                }
+//                eventType = xpp.next();
+//            }
+//            System.out.println("End document");
+//        }
+//        catch (XmlPullParserException | IOException e)
+//        {
+//            e.printStackTrace();
+//        }
 
 
         dialog.show();
     }
+
+
+
+
+    protected void processScannedData(String scanData){
+        Log.d("TAG",scanData);
+        XmlPullParserFactory pullParserFactory;
+
+        try {
+            // init the parserfactory
+            pullParserFactory = XmlPullParserFactory.newInstance();
+            // get the parser
+            XmlPullParser parser = pullParserFactory.newPullParser();
+
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(new StringReader(scanData));
+
+            // parse the XML
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_DOCUMENT) {
+                    Log.d("TAG","Start document");
+                } else if(eventType == XmlPullParser.START_TAG && DataAttributes.AADHAAR_DATA_TAG.equals(parser.getName())) {
+                    // extract data from tag
+                    //uid
+                    uid = parser.getAttributeValue(null,DataAttributes.AADHAR_UID_ATTR);
+                    //name
+                    name = parser.getAttributeValue(null,DataAttributes.AADHAR_NAME_ATTR);
+                    //gender
+                    gender = parser.getAttributeValue(null,DataAttributes.AADHAR_GENDER_ATTR);
+                    // year of birth
+                    yearOfBirth = parser.getAttributeValue(null,DataAttributes.AADHAR_YOB_ATTR);
+                    // care of
+                    careOf = parser.getAttributeValue(null,DataAttributes.AADHAR_CO_ATTR);
+                    // village Tehsil
+                    villageTehsil = parser.getAttributeValue(null,DataAttributes.AADHAR_VTC_ATTR);
+                    // Post Office
+                    postOffice = parser.getAttributeValue(null,DataAttributes.AADHAR_PO_ATTR);
+                    // district
+                    district = parser.getAttributeValue(null,DataAttributes.AADHAR_DIST_ATTR);
+                    // state
+                    state = parser.getAttributeValue(null,DataAttributes.AADHAR_STATE_ATTR);
+                    // Post Code
+                    postCode = parser.getAttributeValue(null, DataAttributes.AADHAR_PC_ATTR);
+
+
+                    Log.d("DATA", "processScannedData: uid-----------"+uid);
+                    Log.d("DATA", "processScannedData: name-----------"+name);
+                    Log.d("DATA", "processScannedData: gender-----------"+gender);
+                    Log.d("DATA", "processScannedData: yearOfBirth-----------"+yearOfBirth);
+                    Log.d("DATA", "processScannedData: careOf-----------"+careOf);
+                    Log.d("DATA", "processScannedData: villageTehsil-----------"+villageTehsil);
+                    Log.d("DATA", "processScannedData: postOffice-----------"+postOffice);
+                    Log.d("DATA", "processScannedData: district-----------"+district);
+                    Log.d("DATA", "processScannedData: state-----------"+state);
+                    Log.d("DATA", "processScannedData: postCode-----------"+postCode);
+
+                } else if(eventType == XmlPullParser.END_TAG) {
+                    Log.d("TAG","End tag "+parser.getName());
+
+                } else if(eventType == XmlPullParser.TEXT) {
+                    Log.d("TAG","Text "+parser.getText());
+
+                }
+                // update eventType
+                eventType = parser.next();
+            }
+
+            // display the data on screen
+           // displayScannedData();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }// EO function
+
 
 }
